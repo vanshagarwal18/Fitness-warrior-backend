f912b24c5d102d90dd6691e7e7a1c6e0ebc780d1
vanshagarwal18 <vanshagarwal1211@gmail.com>
2022-05-18 23:05:20 +0530

    added Get / route

--- mongo.js
+++ mongo.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const db =process.env.CONNECTION;
+const db = process.env.CONNECTION;
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      ssl: { rejectUnauthorized: false },
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
